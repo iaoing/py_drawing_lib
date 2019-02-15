dc6ed5ef54c9655b0b4703ca1071185330606f32
bing_surface_pro <aijiaobing@gmail.com>
2019-02-15 17:25:36 -0500

    lib

--- line/1.py
+++ line/1.py
@@ -9,7 +9,7 @@ from matplotlib.ticker import FuncFormatter, MaxNLocator
 def get_data(fname):
 	fd = open(fname, "r");
 	line = fd.readline()
-	y = [ [] for i in range(2)]
+	y = [ [] for i in range(8)]
 	flag = -1;
 	while(line):
 		if(line[0] == '#' and line[1] == '#'):
@@ -37,7 +37,8 @@ if __name__ == '__main__':
 
 
 	y = get_data("tmp.dat.txt");
-	y_lb = ['leveldb', 'hash_db-128']
+	y_lb = ['leveldb', 'hash_db-1', 'hash_db-8', 'hash_db-16', 'hash_db-32',
+	 'hash_db-64', 'hash_db-128', 'hash_db-256']
 
 	def format_fn(tick_val, tick_pos):
 	    if int(tick_val) in xs:
@@ -51,17 +52,21 @@ if __name__ == '__main__':
 	# ax.xaxis.set_major_formatter(FuncFormatter(format_fn))
 	# ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 
+	l_type = ['+-', 'o-', 'x-', '*-', '>-', '<-', '^-', 'v-']
+
 	for i in range(len(y)):
 		if(i == 0):
 			ax.plot(x_lb, y[i], 'b+-', linewidth = 1.5,label = y_lb[i])
 		else:
-			ax.plot(x_lb, y[i], 'r*-', linewidth = 1.5,label = y_lb[i])
+			ax.plot(x_lb, y[i], '-', linewidth = 1.5,label = y_lb[i])
+		# ax.plot(x_lb, y[i], l_type[i], linewidth = 1.5,label = y_lb[i])
 
 	plt.grid(True)
 	plt.legend(loc = 0) #图例位置自动
 	plt.axis('tight')
 	plt.xlim((0, 1))
-	plt.xlabel('read percentage')
+	plt.xlabel('get percentage')
 	plt.ylabel('time (second)')
-	plt.title('random read 7,352,941 key-value pairs from 7,352,941 pairs\nwhich value size is 128 Bytes')
+	# plt.title('random read 992,063 key-value pairs from 992,06 pairs\nwhich value size is 1000 Bytes')
+	plt.title('random get 7,352,941 key-value pairs which value size is 128 Bytes')
 	plt.show()
